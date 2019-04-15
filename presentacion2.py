730ee375aa20510376a73633bd4b569de1d9dbc4
Daniel T <danieltinizaray@gmail.com>
2019-04-15 16:43:44 -0500

    presentacion-2

---
+++ presentacion2.py
@@ -0,0 +1,13 @@
+'''
+    Este es mi tercer ejemplo
+    en el lenguaje python
+
+    Ingreso de informacion por teclado
+
+    @daniel-alejandro-t
+'''
+
+nombre = input("Ingrese por favor su nombre: ")
+apellido = input("Ingrese por favor su apellido: ")
+
+print("Mi nombre es: " , nombre , "\n\tMi apellido es:\n" ,  apellido)
